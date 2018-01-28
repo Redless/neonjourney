76abe191f477028fa5a226c22e320ce3f13af039
Gabriel Brookman <brookmangabriel@gmail.com>
2018-01-28 10:29:19 -0700

    added more images and got cards working

--- card.lua
+++ card.lua
@@ -1,10 +1,44 @@
-hand = {{"N"},{"S"},{"E"},{"W"}}
+function card(text)
+  outCard = {text = text}
+  outCard.x = -5
+  outCard.y = 10
+  outCard.xv = 0
+  outCard.yv = 0
+  outCard.xa = 0
+  outCard.ya = 0
+  return outCard
+end
+
+function updateCardPosition(cardToUpdate)
+  if (math.abs(cardToUpdate.xa - cardToUpdate.x) < .2) then
+    cardToUpdate.x = cardToUpdate.xa
+    cardToUpdate.xv = 0
+  else
+    cardToUpdate.x = cardToUpdate.x + cardToUpdate.xv
+  end
+  if (math.abs(cardToUpdate.ya - cardToUpdate.y) < .2) then
+    cardToUpdate.y = cardToUpdate.ya
+    cardToUpdate.yv = 0
+  else
+    cardToUpdate.y = cardToUpdate.y + cardToUpdate.yv
+  end
+end
+
+function dropCard(cardToDrop, x, y)
+  cardToDrop.xa = x
+  cardToDrop.ya = y
+  cardToDrop.xv = (cardToDrop.xa - cardToDrop.x)/15
+  cardToDrop.yv = (cardToDrop.ya - cardToDrop.y)/15
+end
 
 function drawCard(cardToDraw)
-  love.graphics.setColor(255,255,255)
-  rectangle("fill",0,0,10,10)
-  love.graphics.setColor(0,0,0)
-  rectangle("fill",1,1,8,8)
+  love.graphics.setColor(200,200,215)
+  rectangle("fill",cardToDraw.x,cardToDraw.y,2,2)
+  if table.getn(cardToDraw) == 0 then
+
+  else
+
+  end
 end
 
 function drawDeck()
--- main.lua
+++ main.lua
@@ -4,13 +4,24 @@ require("mainsquare")
 require("matrix")
 
 function love.load()
+  northArrow = love.graphics.newImage("assets/north.png")
+  southArrow = love.graphics.newImage("assets/south.png")
+  eastArrow = love.graphics.newImage("assets/east.png")
+  westArrow = love.graphics.newImage("assets/west.png")
   calibrateWindow()
+  hand = {card({"N"}),card({"S"}),card({"E"}),card({"W"})}
+  for i = 1,4 do
+    dropCard(hand[i],.7+2.2*(i-1),12.5)
+  end
 end
 
 function love.draw()
   drawOutline()
   drawMainSquare()
   drawDeck()
+  for i =1,4 do
+    drawCard(hand[i])
+  end
 end
 
 function love.update()
@@ -29,6 +40,9 @@ function love.update()
   if mainsquare.yv < 0 then
     mainsquare.y = mainsquare.y - 1
     mainsquare.yv = mainsquare.yv + 1
+  end
+  for i = 1,4 do
+    updateCardPosition(hand[i])
   end
 end
 
